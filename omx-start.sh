4ca02814792a5dd9c99e3389513515c78ce56fee
Walt Harvey <walt.harv@gmail.com>
2015-08-08 10:28:27 +0000

    Added Youtube playback functionality.

--- omx-start.sh
+++ omx-start.sh
@@ -1,20 +1,37 @@
 ## script to start omxplayer
+controls="$3"
+doublespeed="$2"
+videoToPlay="$1"
 
 # check if player is already running
-ps cax | grep "omxplayer" > /dev/null
+ps cax | grep -q "omxplayer"
 if [ $? -eq 0 ]; then
     sudo killall omxplayer && sudo killall omxplayer.bin
 fi
+
 # delete mkfifo file if exist
-if [ -e $3 ]
+if [ -e "$controls" ]
 then
-    rm $3
+    rm "$controls"
 fi
-mkfifo $3
-omxplayer -o hdmi -b "$1" < $3 &
-echo -n "." > $3 &
+
+mkfifo "$controls"
+
+dir="$(dirname "$controls")"
+
+#echo "$videoToPlay" | grep -q "youtube"
+if [ $? -eq 0 ]; then
+videoToPlay="$(youtube-dl -g "$videoToPlay")"
+
+
+fi
+
+omxplayer -I -o local -b "$videoToPlay" < "$controls" 2>"${dir}/vidinfo" &
+
+
+echo -n "." > "$controls" &
 
 # fix for double play speed at start
-if [ "$2" = "1" ]; then
-    echo -n "1" > $3 &
+if [ "$doublespeed" = "1" ]; then
+    echo -n "1" > "$controls" &
 fi

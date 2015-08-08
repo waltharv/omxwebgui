43b36561f9f5569c9ed54c3b54aad8bb4272f776
Walt Harvey <walt.harv@gmail.com>
2015-08-08 10:30:20 +0000

    Added header that allows javascript on another domain to interact with this server

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+header('Access-Control-Allow-Origin: *');
 
 require(__DIR__."/lib/functions.php");
 require(__DIR__."/lib/Translations.class.php");
